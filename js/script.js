8bb051e71452684bd8a53e76456193e52144d358
Ligalaiz <slavakuz88@gmail.com>
2018-10-27 17:38:18 +0300

    Разметка

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+$('[data-fancybox]').fancybox({
+    smallBtn: "false",
+    buttons: [
+        "close"
+      ],
+    touch: {
+      vertical: false,
+      momentum: false
+    }
+});
+
+$(".owl-carousel").owlCarousel({
+  nav:true,
+  dots:false,
+  items: 1,
+  onChanged: itemCount,
+  onChange: itemsCount,
+  navText: ['','']
+});
+  function itemCount(event) {
+      var item = event.item.index;
+      $('.current-count').text(item + 1);
+  }
+  function itemsCount(event) {
+      var items = event.item.count;
+      $('.size-count').text(items);
+  }
+
+  $( function() {
+    $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+    $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+  });
+
+
+  $( function() {
+      var icons = {
+        header: "ui-icon-circle-arrow-e",
+        activeHeader: "ui-icon-circle-arrow-s"
+      };
+      $( "#accordion" ).accordion({
+        icons: icons
+      });
+      $( "#toggle" ).button().on( "click", function() {
+        if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
+          $( "#accordion" ).accordion( "option", "icons", null );
+        } else {
+          $( "#accordion" ).accordion( "option", "icons", icons );
+        }
+      });
+    } );
